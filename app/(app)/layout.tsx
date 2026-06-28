93b41b51937aa951c88159e811f7b37722ddec77
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-28 16:50:25 -0300

    Add Sidebar component to AppLayout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,1 +1,17 @@
+import Sidebar from "@/components/Sidebar";
 
+export default function AppLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div style={{ display: "flex" }}>
+      <Sidebar />
+
+      <main style={{ flex: 1, padding: 20 }}>
+        {children}
+      </main>
+    </div>
+  );
+}
