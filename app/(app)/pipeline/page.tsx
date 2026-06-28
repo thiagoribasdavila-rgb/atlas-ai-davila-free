78bf52d5ef209526fa366b4c0c157cb3ef3efe0b
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-28 16:57:12 -0300

    Rename column 'Lead Novo' to 'Novo Lead'

--- app/(app)/pipeline/page.tsx
+++ app/(app)/pipeline/page.tsx
@@ -4,7 +4,7 @@ export default function Pipeline() {
       <h1>Pipeline de Vendas</h1>
 
       <div className="kanban">
-        <div className="column">Lead Novo</div>
+        <div className="column">Novo Lead</div>
         <div className="column">Contato</div>
         <div className="column">Proposta</div>
         <div className="column">Fechado</div>
@@ -12,4 +12,3 @@ export default function Pipeline() {
     </div>
   );
 }
-
