1acadb8ba44407336e1cfa8568c239cd6d2a5585
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-28 01:16:45 -0300

    Refactor pipelineOrder to remove LeadStatus type

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -1,6 +1,4 @@
-import { LeadStatus } from "./leads";
-
-export const pipelineOrder: LeadStatus[] = [
+export const pipelineOrder = [
   "novo",
   "contato",
   "visita",
@@ -8,8 +6,3 @@ export const pipelineOrder: LeadStatus[] = [
   "fechado",
   "perdido",
 ];
-
-export function nextStatus(current: LeadStatus): LeadStatus {
-  const index = pipelineOrder.indexOf(current);
-  return pipelineOrder[Math.min(index + 1, pipelineOrder.length - 1)];
-}
