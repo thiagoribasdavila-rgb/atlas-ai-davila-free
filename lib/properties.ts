d36184c030df26669fb91324e3b7133107bdc8a4
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-28 00:29:04 -0300

    Create properties.ts

---
+++ lib/properties.ts
@@ -0,0 +1,14 @@
+export const properties = [
+  {
+    id: "1",
+    title: "Apartamento Moderno",
+    price: 550000,
+    location: "São Paulo",
+  },
+  {
+    id: "2",
+    title: "Cobertura Luxo",
+    price: 1200000,
+    location: "Pinheiros",
+  },
+];
