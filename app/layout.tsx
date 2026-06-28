31364f194ae1a6db4010942833c4cb7688cae61c
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-28 17:37:11 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="pt-BR">
-      <body style={{ margin: 0, fontFamily: "Arial" }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
