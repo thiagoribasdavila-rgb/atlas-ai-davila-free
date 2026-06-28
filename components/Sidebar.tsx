678f1313d5264956078923edfc814ac6e3c331dc
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-28 16:54:16 -0300

    Add Sidebar component for CRM application

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,1 +1,20 @@
+export default function Sidebar() {
+  return (
+    <aside style={{
+      width: 250,
+      background: "#111",
+      color: "#fff",
+      minHeight: "100vh",
+      padding: 20
+    }}>
+      <h2>CRM Imobiliário</h2>
 
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li>Dashboard</li>
+        <li>Leads</li>
+        <li>Imóveis</li>
+        <li>Pipeline</li>
+      </ul>
+    </aside>
+  );
+}
