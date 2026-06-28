7b1788528dd9d665932941353405f31e9b4b740f
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-28 12:27:10 -0300

    Delete app directory

--- app/pipeline/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Pipeline() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>📊 Pipeline CRM</h1>
-      <p>Organização de leads funcionando</p>
-    </main>
-  );
-}
