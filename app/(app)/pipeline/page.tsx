d147cf5fc41d86ca3fee2c46cf3c75552bd13d78
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-28 16:40:06 -0300

    Create Pipeline component with sales stages

--- app/(app)/pipeline/page.tsx
+++ app/(app)/pipeline/page.tsx
@@ -1,1 +1,15 @@
+export default function Pipeline() {
+  return (
+    <div>
+      <h1>Pipeline de Vendas</h1>
+
+      <div className="kanban">
+        <div className="column">Lead Novo</div>
+        <div className="column">Contato</div>
+        <div className="column">Proposta</div>
+        <div className="column">Fechado</div>
+      </div>
+    </div>
+  );
+}
 
