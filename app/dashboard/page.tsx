582daec7cbb30275aea6ad96c11afed21f4a12d5
thiagoribasdavila-rgb <thiagoribasdavila@gmail.com>
2026-06-28 22:38:07 -0300

    Delete app directory

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>CRM D’Avila</h1>
-      <p>Sistema online funcionando ✔</p>
-    </main>
-  );
-}
--- app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Login() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>Login</h1>
-      <p>Acesso ao CRM</p>
-    </main>
-  );
-}
